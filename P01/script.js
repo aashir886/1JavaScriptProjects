cd71bfc8152a30dc1e2b173b8ca2907f11e09c97
aashir886 <aashirbhojani743@gmail.com>
2020-08-25 04:28:24 +0500

    Update script.js
    Created Js code for required check using simple JS

--- P01/script.js
+++ P01/script.js
@@ -2,9 +2,49 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
+
+// All Functions
+// Function to show error
+function showError (input,message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'
+    const small = formControl.querySelector('small');
+    small .innerText = message;
+}
+
+// Function to show success
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// This is an avent listener for the form on submit
 form.addEventListener('submit',function(e) {
     e.preventDefault();
-    console.log(username.value);
+    
+    if ( username.value === '' ) {
+        showError(username,'username is required')
+    }else {
+        showSuccess(username);
+    }
+    if ( email.value === '' ) {
+        showError(email,'email is required')
+    }else {
+        showSuccess(email);
+    }
+    
+
+    if ( password.value === '' ) {
+        showError(password,'password is required')
+    }else {
+        showSuccess(password);
+    }
+
+    if ( password2.value === '' ) {
+        showError(password2,'confirm password is required')
+    }else {
+        showSuccess(password2);
+    }
 })
