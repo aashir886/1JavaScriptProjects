99cd72f901194156cd90e838022b3d01edf231d5
aashir886 <aashirbhojani743@gmail.com>
2020-09-02 05:20:31 +0500

    Compeleted project 2 JS
    Some changes all projects and
    - update UI selected seats
    - Save data to local storage
    - Build UI with saved data from local storage

--- p02/script.js
+++ p02/script.js
@@ -5,64 +5,58 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
+// pull data from local storage to builed UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach( (seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
 
 
-// Function to update counts
+//function to update counts
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
     const countSelectedSeats = selectedSeats.length;
+
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const seatsIndex = [... selectedSeats].map(function(seat) {
-        return [...seats].indexOf()
-    });
-
-    console.log(seatsIndex);
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     count.innerText = countSelectedSeats;
     total.innerText = ticketPrice * countSelectedSeats;
 }
 
-// Event Listner for change on Select Movie Dropdown
+
+// function to save the selected movie and its price
+function setMovieData(movieIndex,moviePrice) {
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice',moviePrice);
+}
+
+// Event listner for change on select movie Dropdown
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
+
+
+
 
 // Event Listner for Click on Available Seats
 container.addEventListener('click', (e) => {
@@ -72,3 +66,6 @@ container.addEventListener('click', (e) => {
     }
 })
 
+
+// Calculate initial number of seats and total price
+updateSelectedCount();
